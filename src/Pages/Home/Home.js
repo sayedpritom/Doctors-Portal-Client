ad06328742815ea33bd8aa9e7d525fbd136925c4
unknown <sayedpritom999@gmail.com>
2022-05-16 21:57:02 +0600

    implemented navbar, banner, info

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+import clock from '../../assets/icons/clock.svg';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Info></Info>
+        </div>
+    );
+};
+
+export default Home;
